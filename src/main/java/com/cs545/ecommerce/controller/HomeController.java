9672d120dd2818dae6fe92b5d816485a3f5147a2
HamadaGabrIbrahim <Hamada.G.Ibrahim@gmail.com>
2016-07-13 11:31:02 -0500

    home controller

--- src/main/java/com/cs545/ecommerce/controller/HomeController.java
+++ src/main/java/com/cs545/ecommerce/controller/HomeController.java
@@ -11,7 +11,7 @@ import com.cs545.ecommerce.domain.Category;
 import com.cs545.ecommerce.domain.Product;
 import com.cs545.ecommerce.repository.impl.InMemoryCategoryRepository;
 import com.cs545.ecommerce.service.ProductService;
-  
+
 @Controller
 public class HomeController {
 	@Autowired
@@ -19,19 +19,19 @@ public class HomeController {
 	@Autowired
 	private InMemoryCategoryRepository rep;
 
- 
-	@RequestMapping({"/","/home"})
+	@RequestMapping({ "/", "/home" })
 	public String welcome(Model model) {
-		
-		List<Category> categories=rep.getAllCategories();
-		List<Product> homeProducts=pService.getAllProducts();
-		
+
+		List<Category> categories = rep.getAllCategories();
+		List<Product> homeProducts = pService.getAllProducts();
+
 		model.addAttribute("categories", categories);
-	  
-	    model.addAttribute("homeProducts", homeProducts);
+
+		model.addAttribute("homeProducts", homeProducts);
+
 		model.addAttribute("pageToRender", "jsp/home.jsp");
-		
+
 		return "UI/template";
 	}
- 
+
 }
